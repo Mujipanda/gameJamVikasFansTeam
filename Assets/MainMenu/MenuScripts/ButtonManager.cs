64b772b27c519e22e1c99581b6e7f29522b16d67
Matt <matty01.mw@gmail.com>
2024-05-20 12:32:41 +0100

    Menus

---
+++ Assets/MainMenu/MenuScripts/ButtonManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonManager : MonoBehaviour
+{
+     
+
+    public void ChangeScene(string newGameLevel)
+
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+
+    public void QuitToDesktop()
+    {
+        Application.Quit();
+        Debug.Log("Quiting to Desktop");
+    }
+
+}
