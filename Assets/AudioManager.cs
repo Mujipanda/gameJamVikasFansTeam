b001559f74d18176b888b01006ab4d7baa6b43e3
Matt <matty01.mw@gmail.com>
2024-05-21 13:21:37 +0100

    Audio

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public Button btnOn;
+    public Button btnOff;
+
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        btnOn = GetComponent<Button>();
+        btnOff = GetComponent<Button>();
+
+        btnOn.onClick.AddListener(() => PlayAudio());
+        btnOff.onClick.AddListener(() => StopAudio());
+
+        audioSource = GameObject.Find("bg").GetComponent<AudioSource>();
+    }
+
+    void PlayAudio()
+    {
+        audioSource.volume = 0.5f;
+    }
+
+    void StopAudio()
+    {
+        audioSource.volume = 0f;
+    }
+}
