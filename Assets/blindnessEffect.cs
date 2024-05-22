e121367cc7eea0243141a7596629e86c91b9c39d
Muji <rasmusiam@gmail.com>
2024-05-22 13:42:44 +0100

    Blindness effect added

--- Assets/blindnessEffect.cs
+++ Assets/blindnessEffect.cs
@@ -1,18 +1,73 @@
+
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class blindnessEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private PostProcessVolume fxVolume;
+    Vignette vignet;
+
+    [SerializeField, Range(1,10)]
+    private int effectDuration;
+    [SerializeField, Range(0.2f, 1)]
+    private float vignetIntensity;
+
+    private void Start()
     {
-        
+        vignet = ScriptableObject.CreateInstance<Vignette>();
+        vignet.enabled.Override(true);
+        vignet.intensity.Override(1f);
+
+        fxVolume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, vignet);
+        blindEffect();
+    }
+    private void Update()
+    {
+        //vignet.intensity.value = Mathf.Sin(Time.realtimeSinceStartup);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void blindEffect()
     {
+        StartCoroutine(VignetEffect());
+    }
+    private void OnDestroy()
+    {
+        RuntimeUtilities.DestroyVolume(fxVolume,true, true);
+    }
+
+
+    private IEnumerator VignetEffect()
+    {
+        yield return null;
+        float elapedTime = 0;
+        float duration = 2;
+        while (elapedTime < duration)
+        {
+
+            
+            float t = elapedTime / duration;
+
+            vignet.intensity.value = Mathf.Lerp(0, vignetIntensity, t);
+
+            elapedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
         
+        yield return new WaitForSeconds(effectDuration);
+
+        elapedTime = 0;
+        
+        while (elapedTime < duration)
+        {
+            float t = elapedTime / duration;
+
+            vignet.intensity.value = Mathf.Lerp(vignetIntensity, 0, t);
+
+            elapedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
